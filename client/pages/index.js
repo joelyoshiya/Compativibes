c4eb1eb3eaa07ccc8876703d47ffac691babbfff
Joel Yoshiya Foster <jyfoster@wisc.edu>
2022-11-21 15:36:31 -0600

    attempted auth/fetch spotify api in index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,69 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+//require('dotenv').config(); // to access the .env file
+//console.log(process.env)
+
 
 export default function Home() {
+  // constants used for authentication
+  const CLIENT_ID = "2ebb706e0c834dafbdcf7c190560cf8e"
+  // const CLIENT_SECRET = process.env.CLIENT_SECRET
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+  }, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        params: {
+            q: searchKey,
+            type: "artist"
+        }
+    })
+    // console.log(data.artists.items)
+    setArtists(data.artists.items)
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+        <div key={artist.id}>
+            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+            {artist.name}
+        </div>
+    ))
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,6 +80,17 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
+          <br/>
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+          <br/>
+          <button onClick={logout}>Logout</button>
+          <br/>
+          <form onSubmit={searchArtists}>
+            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+            <button type={"submit"}>Search</button>
+          </form>
+          <br/>
+          {renderArtists}
         </p>
 
         <div className={styles.grid}>
